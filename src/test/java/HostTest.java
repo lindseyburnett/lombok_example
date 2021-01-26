5be170c7059a79fcd5f7d2963745b7b655e32066
Lindsey Burnett <lburnett@redhat.com>
2021-01-26 09:21:55 -0500

    Example of some useful lombok annotations

---
+++ src/test/java/HostTest.java
@@ -0,0 +1,50 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * Copyright (c) 2021 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+class HostTest {
+
+  @Test
+  void testEquality() {
+
+    Host host0 = new Host();
+    host0.setCoreCount(0);
+    host0.setDisplayName("example.displayname.com");
+    host0.setSocketCount(0);
+
+    Host host1 = new Host();
+    host1.setCoreCount(0);
+    host1.setDisplayName("example.displayname.com");
+    host1.setSocketCount(0);
+
+    assertEquals(host0, host1);
+  }
+
+  @Test
+  void testToStringFunction() {
+    Host host0 = new Host();
+    host0.setCoreCount(2);
+    host0.setDisplayName("example.displayname.com");
+    host0.setSocketCount(4);
+  }
+}
---
+++ src/test/java/HypervisorTest.java
@@ -0,0 +1,50 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ * Copyright (c) 2021 Red Hat, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Red Hat trademarks are not licensed under GPLv3. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+class HypervisorTest {
+
+  @Test
+  void sanityCheck() {
+    assertTrue(true);
+  }
+
+  @Test
+  void testBuilder() {
+
+    Hypervisor expected = Hypervisor.builder().displayName("redhat.com").numberOfGuests(5).build();
+    Hypervisor actual = Hypervisor.builder().displayName("redhat.com").numberOfGuests(5).build();
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void testToString() {
+    Hypervisor example = Hypervisor.builder().displayName("redhat.com").numberOfGuests(5).build();
+
+    String expected = "Hypervisor(displayName=redhat.com, numberOfGuests=5)";
+    String actual = example.toString();
+
+    assertEquals(expected, actual);
+  }
+}
